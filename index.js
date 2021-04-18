483c20f944990f167ddb309f824146a5de7dff74
yisraelgurkow <yisraelg3@gmail.com>
2021-04-18 01:16:22 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Yisrael is the champion';
+document.body.appendChild(newHeader);
